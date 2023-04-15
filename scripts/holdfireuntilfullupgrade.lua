7c6862a267dcc094077f637f8685bb04d61953bc
DrCactusTF2 <zwright@mwcc.edu>
2023-04-15 00:49:54 -0400

    Hell-Met requests + Overclock parity update
    Bigrock Reverses
    - Updated PDQ Sentry
    -Fixed various exploits
    
    Hell-met Various
    - Added overclocks per hell-met request
    
    Overclocks
    - Parity update with current versions

---
+++ scripts/holdfireuntilfullupgrade.lua
@@ -0,0 +1,85 @@
+local holdfirePlayers = {}
+
+local BASE_CLIP = 4
+
+function EnableHoldFire(_, activator)
+	local handleIndex = activator:GetHandleIndex()
+
+	local primary = activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY)
+
+	local lastClip = primary.m_iClip1
+	local canAttack = true
+
+	local logic
+	logic = timer.Create(0.1, function()
+		if not IsValid(activator) then
+			timer.Stop(logic)
+			holdfirePlayers[handleIndex] = nil
+			return
+		end
+
+		local clipBonusMult = primary:GetAttributeValueByClass("mult_clipsize", 1)
+		local clipBonusAtomic = primary:GetAttributeValueByClass("mult_clipsize_upgrade_atomic", 0)
+
+		local maxClip = (BASE_CLIP * clipBonusMult) + clipBonusAtomic
+
+		local curClip = primary.m_iClip1
+
+		-- print(curClip, maxClip)
+		if curClip == maxClip then
+			canAttack = true
+		elseif curClip == 0 then
+			canAttack = false
+		end
+
+		if not canAttack then
+			primary:SetAttributeValue("no_attack", 1)
+		else
+			primary:SetAttributeValue("no_attack", nil)
+		end
+
+		lastClip = curClip
+	end, 0)
+
+	local callbacks = {}
+	local data = {
+		LastClip = lastClip,
+
+		Callbacks = callbacks,
+		LogicTimer = logic
+	}
+
+	callbacks.Release = activator:AddCallback(ON_KEY_RELEASED, function(_, key)
+		if key ~= IN_ATTACK then
+			return
+		end
+
+		canAttack = false
+	end)
+	callbacks.Spawned = activator:AddCallback(ON_SPAWN, function()
+		if activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY) == primary then
+			return
+		end
+		DisableHoldFire(_, activator)
+	end)
+
+	holdfirePlayers[handleIndex] = data
+end
+
+function DisableHoldFire(_, activator)
+	local index = activator:GetHandleIndex()
+
+	local data = holdfirePlayers[index]
+	activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):SetAttributeValue("no_attack", nil)
+
+	timer.Stop(holdfirePlayers[index].LogicTimer)
+	for _, callbackId in pairs(data.Callbacks) do
+		activator:RemoveCallback(callbackId)
+	end
+
+	holdfirePlayers[index] = nil
+end
+
+-- function OnPlayerDisconnected(player)
+-- 	DisableHoldFire(nil, player)
+-- end
---
+++ scripts/lua_knockback_rage.lua
@@ -0,0 +1,107 @@
+local RAGE_DURATION = 10
+local UPDATE_INTERVAL = 0.015
+
+local knockbackHandles = {}
+
+local RAGE_OVERRIDES_APPLY = {
+    ["throwable fire speed"] = function(activator)
+        activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):SetAttributeValue("Set DamageType Ignite", 5)
+    end,
+    ["throwable damage"] = function(activator)
+        activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):SetAttributeValue("rocket specialist", 1)
+    end
+}
+local RAGE_OVERRIDES_REMOVE = {
+    ["throwable fire speed"] = function(activator)
+        activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):SetAttributeValue("Set DamageType Ignite", nil)
+    end,
+    ["throwable damage"] = function(activator)
+        activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):SetAttributeValue("rocket specialist", nil)
+    end
+}
+
+local function lerp(a, b, t)
+	return a + (b - a) * t
+end
+
+local function applyKnockbackRageEffect(activator)
+    for attr, func in pairs(RAGE_OVERRIDES_APPLY) do
+        if activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):GetAttributeValue(attr) then
+            func(activator)
+            break
+        end
+    end
+end
+
+local function removeKnockbackRageEffect(activator)
+    for attr, func in pairs(RAGE_OVERRIDES_REMOVE) do
+        if activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):GetAttributeValue(attr) then
+            func(activator)
+            break
+        end
+    end
+end
+
+function KnockbackRageStart(_, activator)
+    local handleIndex = activator:GetHandleIndex()
+
+    local draining = false
+    local fakeMeter = 0
+    local meterLerp = 0
+
+    knockbackHandles[handleIndex] = timer.Create(UPDATE_INTERVAL, function ()
+        if not IsValid(activator) then
+            KnockbackRageStop(nil, nil, handleIndex)
+            return
+        end
+
+        if draining then
+            -- fakeMeter = math.max(fakeMeter - (UPDATE_INTERVAL * RAGE_DURATION) * 10, 0)
+            meterLerp = meterLerp + (UPDATE_INTERVAL / RAGE_DURATION * 2)
+            fakeMeter = math.max(100 - lerp(0, 100, meterLerp), 0)
+
+            activator:SetFakeSendProp("m_flRageMeter", fakeMeter)
+
+            if fakeMeter <= 0 then
+                activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):SetAttributeValue("increase buff duration HIDDEN", nil)
+                activator:ResetFakeSendProp("m_bRageDraining")
+                activator:ResetFakeSendProp("m_flRageMeter")
+                activator.m_flRageMeter = 0
+
+                fakeMeter = 0
+                meterLerp = 0
+                draining = false
+
+                removeKnockbackRageEffect(activator)
+            end
+
+            return
+        end
+
+        if activator.m_bRageDraining == 0 then
+            return
+        end
+
+        activator:GetPlayerItemBySlot(LOADOUT_POSITION_PRIMARY):SetAttributeValue("increase buff duration HIDDEN", 100)
+        activator:SetFakeSendProp("m_bRageDraining", 1)
+        activator.m_bRageDraining = 0
+        activator.m_flRageMeter = -100000
+        fakeMeter = 100
+        meterLerp = 0
+        draining  = true
+
+        applyKnockbackRageEffect(activator)
+    end, 0)
+end
+
+function KnockbackRageStop(_, activator, handle)
+    if activator then
+        activator:ResetFakeSendProp("m_bRageDraining")
+        activator:ResetFakeSendProp("m_flRageMeter")
+    end
+
+    local handleIndex = handle or activator:GetHandleIndex()
+
+    timer.Stop(knockbackHandles[handleIndex])
+    knockbackHandles[handleIndex] = nil
+end
